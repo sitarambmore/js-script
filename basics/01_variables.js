12d40f4910e0469b91caa5429c59bd7b66793707
Sitaram More <108779328+sitarambmore@users.noreply.github.com>
2025-03-06 18:53:10 +0000

    added datatype files

---
+++ basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sitaram@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+accountState;
+// accountId = 2 //This is not allowed
+console.log(accountId);
+
+// perfer not to use Var keyword for declaring the varible
+
+accountEmail ="raj@gmail.com"
+
+accountCity= "Channai" 
+console.table();
---
+++ basics/2_datatype.js
@@ -0,0 +1,16 @@
+console.log("sitaram")
+
+let name ="sitaram"
+let age = 28
+let isLoggedIn = false
+
+// Number =>2 to power 53
+// bigint
+// string => ""
+// boolean=>True or False
+// null =>Its standalone values 
+// undefined => yet not value defined
+// symbol => to find the unique 
+
+// object
+
